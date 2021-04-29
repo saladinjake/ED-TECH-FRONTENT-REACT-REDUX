46a1e2354ff394213effa0be4e5b13414e164537
unknown <juwavictor@gmail.com>
2021-04-30 00:31:38 +0200

    Ui fixes  3 layered menu navigation

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -16,6 +16,8 @@ import "./components.css";
 import { //CATEGORIES,
  PACES,  AUTHLINKS } from "./data";
 
+ import "./nav.css"
+
 import { getCategories } from "services/category";
 import toast from "react-hot-toast";
 
@@ -74,15 +76,361 @@ const NavBar = ({
 
   return (
     <Fragment>
-      <nav className="desktop">
-        <figure className="logo">
+
+
+
+
+   <section class="menu-area" >
+    <div class="container-xl">
+        <div class="row">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+
+                 <ul class="mobile-header-buttons">
+                        <li><a class="mobile-nav-trigger" href="#mobile-primary-nav">Menu<span></span></a></li>
+                        <li><a class="mobile-search-trigger" href="#mobile-search">Search<span></span></a></li>
+                    </ul>
+
+                    <a href="#" class="navbar-brand">
+                        
+                         <img src={questence} alt="" width="171px" height="35" />
+                    </a>
+
+
+
+                     <div class="main-nav-wrap">
+                                    <div class="mobile-overlay"></div>
+
+                                    <ul class="mobile-main-nav">
+                                      <div class="mobile-menu-helper-top"></div>
+
+                                      <li class="has-children">
+                                        <a href="">
+                                          <i class="fas fa-th d-inline"></i>
+                                          <span>Courses</span>
+                                          <span class="has-sub-category"><i class="fas fa-angle-right"></i></span>
+                                        </a>
+
+                                        <ul class="category corner-triangle top-left is-hidden pb-0">
+                                          <li class="go-back"><a href=""><i class="fas fa-angle-left"></i>Menu</a></li>
+
+                                           {categories.length > 0 &&
+                    categories.map((item, i) => {
+                      return (
+                       
+
+                           <li class="has-children">
+                                           <Link
+                            
+                            to={`${process.env.PUBLIC_URL}/courses/category/${item.id}`}
+                          >
+                                            <span class="icon"><i class="fas fa-chess"></i></span>
+                                            <span> {item.name}</span>
+                                            <span class="has-sub-category"><i class="fas fa-angle-right"></i></span>
+                                          </Link>
+                                          <ul class="sub-category is-hidden">
+                                          {item.subcategories.length > 0  && item.subcategories.map( cat =>{
+                                              return (
+                                              <Fragment>
+                                            
+                                            <li class="go-back-menu"><a href="#">
+                                               <i class="fas fa-angle-left"></i>Menu</a>
+                                            </li>
+                                            <li class="go-back">
+                                              <a href="#">
+                                                <i class="fas fa-angle-left"></i>
+                                                <span class="icon">
+                                                     <i class="fas fa-chess"></i>
+                                                 </span>
+                                              </a>
+                                            </li>
+                                                    <li><Link to={`${process.env.PUBLIC_URL}/courses/category/${cat.id}`}>  {cat.name}</Link></li>
+                                                    
+                                                </Fragment>    
+                                             
+
+                                              )
+
+                                          }) }
+                                          </ul>
+                                          
+                                          
+                        </li>
+                      );
+                    })}
+
+
+
+                   
+
+
+
+                                             
+                                          
+                                      <li class="all-category-devided mt-2 mb-0 p-0">
+                                      <Link to={ process.env.PUBLIC_URL + "/courses" } class="py-3">
+                                        <span class="icon"><i class="fa fa-align-justify"></i></span>
+                                        <span>All courses</span>
+                                      </Link>
+                                    </li>
+
+                                    </ul>
+                                  </li>
+
+                                  <div class="mobile-menu-helper-bottom"></div>
+                                  </ul>
+                                  </div>
+
+
+                        <div class="input-group search-box mobile-search">
+                                         <div className="search__form">
+                            <input
+                              type="text"
+                              name="search"
+                              id="search"
+                              placeholder="Search for a course"
+                            />
+                            <button onClick={handleSearch} type="button">
+                              Search
+                              <svg
+                                width="14"
+                                height="14"
+                                viewBox="0 0 14 14"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M10.0057 8.80503H9.37336L9.14923 8.58891C9.93368 7.67639 10.4059 6.49171 10.4059 5.20297C10.4059 2.32933 8.07662 0 5.20297 0C2.32933 0 0 2.32933 0 5.20297C0 8.07662 2.32933 10.4059 5.20297 10.4059C6.49171 10.4059 7.67639 9.93368 8.58891 9.14923L8.80503 9.37336V10.0057L12.8073 14L14 12.8073L10.0057 8.80503ZM5.20297 8.80503C3.20983 8.80503 1.60091 7.19611 1.60091 5.20297C1.60091 3.20983 3.20983 1.60091 5.20297 1.60091C7.19611 1.60091 8.80503 3.20983 8.80503 5.20297C8.80503 7.19611 7.19611 8.80503 5.20297 8.80503Z"
+                                  fill="white"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+
+
+
+                       {/* <div class="instructor-box menu-icon-box">
+                            <div class="icon">
+                                <a href="#user" >Link here</a>
+                            </div>
+                        </div>
+                    
+                        <div class="instructor-box menu-icon-box">
+                            <div class="icon">
+                                <a href="mycourses" >My courses</a>
+                            </div>
+                        </div>*/}
+
+
+
+
+
+
+                         <div class="wishlist-box menu-icon-box" id="wishlist_items">
+                                <div class="icon">
+                                    <a href=""><i class="far fa-heart"></i></a>
+                                    <span class="number">1</span>
+                                </div>
+                                <div class="dropdown course-list-dropdown corner-triangle top-right">
+                                    <div class="list-wrapper">
+                                        <div class="item-list">
+                                            <ul>
+                                                                    <li>
+                              <div class="item clearfix">
+                                  <div class="item-image">
+                                      <a href="">
+                                          <img src="#uploads/thumbnails/course_thumbnails/course_thumbnail_default_26.jpg" alt="" class="img-fluid" />
+                                      </a>
+                                  </div>
+                                  <div class="item-details">
+                                      <a href="#home/course/questence-ss1-mathematics/26">
+                                          <div class="course-name">Questence SS1 Mathematics</div>
+                                          <div class="instructor-name">
+                                              By Instructor-01 Questence                                    
+                                          </div>
+
+                                          <div class="item-price">
+                                          <span class="current-price">Free</span>
+                                  </div>
+                                </a>
+                                                    <button>Already purchased</button>
+                                            </div>
+                                                        </div>
+                                                    </li>
+                                                            </ul>
+                                        </div>
+                                        <div class="dropdown-footer">
+                                            <a href="#home/my_wishlist">Go to wishlist</a>
+                                        </div>
+                                    </div>
+                                    <div class="empty-box text-center d-none">
+                                        <p>Your wishlist is empty.</p>
+                                        <a href="">Explore courses</a>
+                                    </div>
+                                </div>
+                        </div>
+
+
+
+
+
+
+                        <div class="cart-box menu-icon-box" id="cart_items">
+                          <div class="icon">
+                              <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                              <span class="number">{cart !== undefined && `(${cart?.length})`}</span>
+                          </div>
+
+
+                          <div class="dropdown course-list-dropdown corner-triangle top-right" >
+                              <div class="list-wrapper">
+                                  <div class="item-list">
+                                      <ul>
+                                                      </ul>
+                                  </div>
+                                  <div class="dropdown-footer">
+                                      <div class="cart-total-price clearfix">
+                                          <span>Total:</span>
+                                          <div class="float-right">
+                                              <span class="current-price"></span>
+                                             
+                                          </div>
+                                      </div>
+                                      <a href="#">Go to cart</a>
+                                  </div>
+                              </div>
+                              <div class="empty-box text-center d-none">
+                                  <p>Your cart is empty.</p>
+                                  <a href="">Keep Shopping</a>
+                              </div>
+                          </div>
+
+
+
+                     </div>
+
+
+
+
+
+                      <div class="user-box menu-icon-box">
+                        <div class="icon">
+                            <a href="javascript::">
+                            <img
+                      src={
+                        process.env.PUBLIC_URL + "/assets/images/questone.jpg"
+                      }
+                      alt=""
+                      width="20px"
+                    />
+                        </a>
+                    </div>
+                    <div class="dropdown user-dropdown corner-triangle top-right">
+                        <ul class="user-dropdown-menu">
+
+
+                            
+
+                            
+                            
+                            
+                            
+                            
+                            
+                        {!isAuthenticated ? (
+            <Fragment>
+              <li>
+                <Link to={process.env.PUBLIC_URL +"/login" } className="auth outline">
+                  Log In
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="btnMobileFull"
+                  onClick={() => history.push("register")}
+                >
+                  Sign Up
+                </button>
+              </li>
+            </Fragment>
+          ) : (
+            <Fragment>
+
+            <li class="dropdown-user-info">
+                                <a href="">
+                                    <div class="clearfix">
+                                        <div class="user-image float-left">
+                                             <img
+                      src={
+                        process.env.PUBLIC_URL + "/assets/images/questone.jpg"
+                      }
+                      alt=""
+                      width="20px"
+                    />
+                                        </div>
+                                        <div class="user-details">
+                                            <div class="user-name">
+                                                <span class="hi">Hi,</span>
+                                                 {`${user?.first_name} ${user?.last_name}`}                                          </div>
+                                            <div class="user-email">
+                                                <span class="email">${user?.email}`}</span>
+                                                <span class="welcome">Welcome back</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+
+
+
+              
+
+                
+           
+            </Fragment>
+          )}
+        </ul>
+
+
+                     
+                    </div>
+                </div>
+
+
+
+             
+                
+
+
+                 
+
+
+
+
+
+  </nav>
+        </div>
+    </div>
+</div>
+</section>
+
+
+
+
+
+      <nav className="desktop" style={{height:"40px"}}>
+        {/*<figure className="logo">
           <Link to="/">
             <img src={questence} alt="" width="171px" />
           </Link>
-        </figure>
+        </figure>*/}
 
         <ul className="mainNav" ref={mobileNav}>
-          <li className="NavHover">
+                        
+{/*   <li className="NavHover">
             <div className="parent">
               <span>Courses and Programs</span>
               <Dropdown />
@@ -116,8 +464,7 @@ const NavBar = ({
                     })}
                 </ul>
               </li>
-              
-{/*              <li className="ParentDropDown__item">
+           <li className="ParentDropDown__item">
                 <Link
                   className="DropDown__link"
                   to={process.env.PUBLIC_URL + "/courses"}
@@ -149,7 +496,7 @@ const NavBar = ({
                       );
                     })}
                 </ul>
-              </li> */}
+              </li> 
 
               <li className="ParentDropDown__item">
                 <span>By Learning Style</span>
@@ -167,7 +514,7 @@ const NavBar = ({
                 </ul>
               </li>
             </ul>
-          </li>
+          </li>*/}
 
           <li className="NavHover">
             <div className="parent">
@@ -204,7 +551,7 @@ const NavBar = ({
 
         
 
-          <li className="search__group">
+          {/*<li className="search__group">
             <div className="search__form">
               <input
                 type="text"
@@ -235,6 +582,8 @@ const NavBar = ({
                                                     </Link>
                                                     
                                                 </li>
+
+           */}
 
 
           {!isAuthenticated ? (
--- src/components/oldhero.js
+++ src/components/oldhero.js
@@ -1,25 +1,7 @@
-import React , { useEffect } from 'react';
-import "./styles/hero.scss";
-import { Container, Row, Col } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
-
-
-import "./hero.css";
-const Hero = () => {
-  let history = useHistory();
-useEffect(() => {
-
-
-})
-  
-  return (
-
-
-    <section className="hero-area">
+ {/*<section className="hero-area">
       <Container>
       <Row>
         <Col lg="7">
-
           <div className="hero-area__left">
             <h2>
               Accelerate your quest,<br></br>
@@ -29,19 +11,6 @@ useEffect(() => {
             Acquire new knowledge and skills, train for certifications, diplomas and degrees from world-class institutions at your 
             own pace and space.
             </p>
-
-             <ul class="contact">
-      <li><a href="mailto:jonas@badalic.com">Login</a></li>
-          <li>
-          <a target="_top"href="https://codepen.io/JonasB/">Signup</a>
-        </li>
-        <li>
-          <a target="_top" href="https://twitter.com/JonasBadalic">Find courses</a></li>
-        <li><a target="_top" href="https://si.linkedin.com/in/jonasbadalic">About</a></li>
-      </ul>
-
-
-
             <div className="cta">
               <button onClick={() => history.push('/courses')}>Find Courses</button>
               <button className="outline" onClick={() => history.push('/register')}>Sign Up</button>
@@ -58,27 +27,7 @@ useEffect(() => {
         />
       </figure>
       </Col>
-
-     
-
       </Row>
-
       </Container>
   
-  
-
-  
-    
-
-    
-  
-     </section>
-
-
-
-
-   
-  );
-};
-
-export default Hero;
+    </section>*/}
