d3cf5171bcf8492d482209fe2c86c7e81a35fad5
unknown <juwavictor@gmail.com>
2021-04-30 16:39:43 +0200

    fixes on grid

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,55 +1,33 @@
-import React , { useEffect } from 'react';
+import React from "react";
 import "./styles/hero.scss";
 import { Container, Row, Col } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 
-
-import "./hero.css";
 const Hero = () => {
   let history = useHistory();
-useEffect(() => {
 
-
-})
   
   return (
-
-
-    <section className="hero-area" >
+    <section className="hero-area">
       <Container>
       <Row>
         <Col lg="7">
-
           <div className="hero-area__left">
             <h2>
               Accelerate your quest,<br></br>
               <span>learn</span> anywhere, anytime.
             </h2>
-            <p style={{color:"#000"}}>
+            <p>
             Acquire new knowledge and skills, train for certifications, diplomas and degrees from world-class institutions at your 
             own pace and space.
             </p>
-
-            {/* <ul class="contact">
-                <li><a href="#">Login</a></li>
-          <li>
-          <a target="_top"href="#">Signup</a>
-        </li>
-        <li>
-          <a target="_top" href="#">Find courses</a></li>
-        <li><a target="_top" href="#">About</a></li>
-      </ul>
-      <br/>  <br/>*/}
-
-
-
             <div className="cta">
               <button onClick={() => history.push('/courses')}>Find Courses</button>
               <button className="outline" onClick={() => history.push('/register')}>Sign Up</button>
             </div>
           </div>
       </Col>
-     
+      {/* <div className="hero-area__right"> */}
       <Col lg="5">
       <figure>
         <img
@@ -59,26 +37,10 @@ useEffect(() => {
         />
       </figure>
       </Col>
-
-     
-
       </Row>
-
       </Container>
-  
-  
-
-  
-    
-
-    
-  
-     </section>
-
-
-
-
-   
+      {/* </div> */}
+    </section>
   );
 };
 
